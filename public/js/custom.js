91a0a7beac6282532b4fca4b3c1265cfa9f412b1
end3rs-army <eric.m.dowty@gmail.com>
2015-05-22 08:43:33 -0600

    initial upload

---
+++ public/js/custom.js
@@ -0,0 +1,13 @@
+function hover_twitter(element) {
+    element.setAttribute('src', 'img/twitter.png');
+}
+function unhover_twitter(element) {
+    element.setAttribute('src', 'img/twitter-grey.png');
+}
+
+function hover_fb(element) {
+    element.setAttribute('src', 'img/fb.png');
+}
+function unhover_fb(element) {
+    element.setAttribute('src', 'img/fb-grey.png');
+}
